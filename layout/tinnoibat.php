a6ee5091b024bcd0abbea0e66fe06e810b500e2a
tanphat10197 <huynhlamtanphat123@gmail.com>
2018-03-20 17:05:32 +0700

    Commit 7
    Hoàn thành phần tour nổi bật

--- layout/tinnoibat.php
+++ layout/tinnoibat.php
@@ -10,26 +10,75 @@
 		<div class="row" style="color:black; margin-top: 40px">
 			<div class="col-md-4" align="center">
 				<div class="card" style="width: 18rem;">
+
 				 <div>
-					<div class="parent" onclick="">
+					<div class="parent">
 				        <div class="child bg-one">
 				            <a href="#"></a>
 				        </div>
 				    </div>
-					<div class="centered row" style="width: 15em">NGŨ HÀNH SƠN-HỘI AN-HUẾ-ĐỘNG THIÊN ĐƯỜNG-BÁN ĐẢO SƠN TRÀ</div>
+					<div class="centered row">NGŨ HÀNH SƠN-HỘI AN-HUẾ-ĐỘNG THIÊN ĐƯỜNG-BÁN ĐẢO SƠN TRÀ</div>
 				 </div>
+
 				  <div class="card-body" style="background-color: rgb(172,214,60); color: white">
-				    <h3 class="card-title">5.500.000 <span>VNĐ</span></h3>
-				    <p class="card-text">Some quick example text to build on the card </p>
-				    <a href="#" class="btn btn-primary">Go somewhere</a>
+				    <h3 class="card-title">5.500.000 <sup>VNĐ</sup></h3>
+				     <p class="card-text" style="color:#343434">
+				     	<img src="Public/images/icon-clock.png" width="20em" title="icon-clock">&nbsp 4 Ngày &nbsp
+				     	<img src="Public/images/icon-lich.png" width="20em" title="icon-lich">&nbsp Liên hệ</p>
+				    <p class="card-text" style="color:#343434">Động Thiên Đường (được mệnh danh là Cung Điện trong lòng núi). Sau khi khám phá vẻ đẹp huyền ảo của những khối thạch nhũ...></p>
+				    <a href="#" class="btn button-book">Book now</a>
 				  </div>
 				</div>
 			</div>
-			<div class="col-md-4">
-				aa
+
+
+			<div class="col-md-4" align="center">
+				<div class="card" style="width: 18rem;">
+
+				 <div>
+					<div class="parent">
+				        <div class="child bg-true">
+				            <a href="#"></a>
+				        </div>
+				    </div>
+					<div class="centered row">CÙ LAO CHÀM</div>
+				 </div>
+
+				  <div class="card-body" style="background-color: rgb(172,214,60); color: white;">
+				    <h3 class="card-title">500.000 <sup>VNĐ</sup></h3>
+				     <p class="card-text" style="color:#343434">
+				     	<img src="Public/images/icon-clock.png" width="20em" title="icon-clock">&nbsp 1 Ngày &nbsp
+				     	<img src="Public/images/icon-lich.png" width="20em" title="icon-lich">&nbsp Hằng ngày</p>
+				    <p class="card-text" style="color:#343434">Năm 2009, khi được UNESSCO công nhận là Khu Dự trữ Sinh quyển Thế giới, Cù Lao Chàm nhanh chóng ...</p>
+				    <a href="#" class="btn button-book">Book now</a>
+				  </div>
+				</div>
 			</div>
-			<div class="col-md-4">
-				aa
+
+
+
+			<div class="col-md-4" align="center">
+				<div class="card" style="width: 18rem;">
+
+				 <div>
+					<div class="parent">
+				        <div class="child bg-three">
+				            <a href="#"></a>
+				        </div>
+				    </div>
+					<div class="centered row">NGŨ HÀNH SƠN-HỘI AN-HUẾ-ĐỘNG THIÊN ĐƯỜNG-BÁN ĐẢO SƠN TRÀ</div>
+				 </div>
+
+				  <div class="card-body" style="background-color: rgb(172,214,60); color: white">
+				    <h3 class="card-title">300.000 <sup>VNĐ</sup></h3>
+				     <p class="card-text" style="color:#343434">
+				     	<img src="Public/images/icon-clock.png" width="20em" title="icon-clock">&nbsp 4 Ngày &nbsp
+				     	<img src="Public/images/icon-lich.png" width="20em" title="icon-lich">&nbsp Liên hệ</p>
+				    <p class="card-text" style="color:#343434">Rừng dừa được ví von như là “Một góc Nam bộ trong lòng Phố cổ.”
+					Hài hòa giữa vùng đất đô thị nhộn nhịp, Phố cổ cũng gắn...</p>
+				    <a href="#" class="btn button-book">Book now</a>
+				  </div>
+				</div>
 			</div>
 		</div>
 	</div>
