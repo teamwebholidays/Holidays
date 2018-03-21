bc38d3798075239abbceb14a48c8fb1d2556b204
Cong Phat <pcphat.web@gmail.com>
2018-03-21 17:41:39 +0700

    80% dat tour

--- layout/head.php
+++ layout/head.php
@@ -9,6 +9,9 @@
     <!-- Bootstrap core CSS -->
     <link href="Public/Bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
 
+    <!-- Font Awesome -->
+    <link rel="stylesheet" type="text/css" href="Public/font/font-awesome/css/font-awesome.min.css" />
+
     <!-- Custom styles for this template -->
     <link href="Public/css/style.css" rel="stylesheet">
 </head>
