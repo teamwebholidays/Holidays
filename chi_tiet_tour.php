550db2e23fea5e530a7086a80f6e3079e314d9c3
tanphat10197 <huynhlamtanphat123@gmail.com>
2018-03-22 12:37:51 +0700

    làm phần chi tiết đặt tour
    làm phần chi tiết đặt tour

---
+++ chi_tiet_tour.php
@@ -0,0 +1,43 @@
+<!doctype html>
+<html lang="en">
+    <!-- head -->
+      <?php
+        include('layout/head.php');
+      ?>
+    <!-- end head -->
+
+    <body>
+        <!-- header -->
+          <?php
+            include('layout/header.php');
+          ?>
+        <!-- end header-->
+
+            <!-- content -->
+
+            <!-- DAT TOUR -->
+              <?php
+                include('layout/dattour.php');
+              ?>
+            <!-- END DAT TOUR -->
+
+            <!-- TOUR NOI BAT -->
+              <?php
+                include('layout/chitiettour.php');
+              ?>       
+            <!-- end content -->
+
+            <!-- footer -->
+              <?php
+                include('layout/footer.php');
+              ?>
+            <!--end footer -->
+           
+           <!-- script -->
+              <?php
+                include('layout/script.php');
+              ?>
+            <!--end script -->
+    </body>
+
+</html>
---
+++ layout/chitiettour.php
@@ -0,0 +1,76 @@
+<div class="container maincttour">
+
+	<div class="row">
+		<h3>Seoul, Daejon</h3>
+	</div>
+
+	<div class="row">
+		<!-- div slideshow -->
+		<div class="col-md-6">
+			<div id="demo" class="carousel slide" data-ride="carousel">
+ 			 	 <!-- Indicators -->
+				  <ul class="carousel-indicators">
+				    <li data-target="#demo" data-slide-to="0" class="active"></li>
+				    <li data-target="#demo" data-slide-to="1"></li>
+				    <li data-target="#demo" data-slide-to="2"></li>
+				  </ul>
+				  
+				  <!-- The slideshow -->
+				  <div class="carousel-inner" >
+				    <div class="carousel-item active">
+				      <img src="public/images/tour_noi_bat/cu-lao-cham.jpg" alt="Los Angeles">
+				    </div>
+				    <div class="carousel-item">
+				      <img src="public/images/tour_noi_bat/ngu-hanh-son.jpg" alt="Chicago" >
+				    </div>
+				    <div class="carousel-item">
+				      <img src="public/images/tour_noi_bat/rung-dua.jpg" alt="New York">
+				    </div>
+				  </div>
+				  
+				  <!-- Left and right controls -->
+				  <a class="carousel-control-prev" href="#demo" data-slide="prev">
+				    <span class="carousel-control-prev-icon"></span>
+				  </a>
+				  <a class="carousel-control-next" href="#demo" data-slide="next">
+				    <span class="carousel-control-next-icon"></span>
+				  </a>
+				</div>
+		</div>
+		<!--end div slideshow -->
+
+		<div class="col-md-6">
+			<div class="row"> 
+				<div class="col-md-4"><p><b>Mã tour:</b></p></div>
+				<div class="col-md-8"><p>NNSGNKR5-001-040618VN-VSCC</p></div>
+			</div>
+
+			<div class="row"> 
+				<div class="col-md-4"><p><b>Khởi hành:</b></p></div>
+				<div class="col-md-4"><p>04/06/2018</p></div>
+				<div class="col-md-4 text-danger"><p><img src="public/images/icon-back-logo/icon-lich.png" title="icon lich" width="10%">Ngày khác</p></div>
+			</div>
+
+			<div class="row"> 
+				<div class="col-md-4"><p><b>Nơi khởi hành:</b></p></div>
+				<div class="col-md-4"><p>VSSC</p></div>
+				<div class="col-md-4"><p>Thời gian:5 ngày</p></div>
+			</div>
+
+			<hr/>
+
+			<div class="row" style="margin-top: 20px;">
+				<div class="col-md-6" align="center" style="border:1px solid #B7B5B5">
+					<h3 class="text-danger">36,990,000 đ</h3>
+
+					<p style="border-top: 1px solid #B7B5B5">Số chổ còn nhận : <span class="text-danger">9</span></p>
+				</div>
+
+				<div class="col-md-6 btndattour">
+					<input type="submit" name="btndat" value="ĐẶT NGAY">
+				</div>
+			</div>
+		</div>
+
+	</div>
+</div>
