20f3a99ee91fcfbe6f9be72be4c94bdbfda63ff0
tanphat10197 <huynhlamtanphat123@gmail.com>
2018-03-24 15:53:16 +0700

    thêm file css font-awesome.min.css để sử dụng các icon
    thêm file css font-awesome.min.css để sử dụng các icon

---
+++ chi_tiet_tin_tuc.php
@@ -0,0 +1,46 @@
+<!doctype html>
+<html lang="en">
+    <!-- head -->
+      <?php
+        include('layout/head.php');
+      ?>
+    <!-- end head -->
+
+    <body>
+        <!-- header -->
+          <?php
+            include('layout/header.php');
+          ?>
+        <!-- end header-->
+
+            <!-- content -->
+
+            <!-- DAT TOUR -->
+              <?php
+                include('layout/dattour.php');
+              ?>
+            <!-- END DAT TOUR -->
+
+            <!-- TOUR TIN TỨC DU LỊCH-->
+             <?php
+                include('layout/chitiettintuc.php');
+              ?>
+            <!-- END TOUR TIN TỨC DU LỊCH -->
+
+
+            <!-- end content -->
+
+            <!-- footer -->
+              <?php
+                include('layout/footer.php');
+              ?>
+            <!--end footer -->
+           
+           <!-- script -->
+              <?php
+                include('layout/script.php');
+              ?>
+            <!--end script -->
+    </body>
+
+</html>
