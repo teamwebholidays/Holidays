5e0c71c8fcb1517e1f3800e6c7fd3198387d6e7e
Cong Phat <pcphat.web@gmail.com>
2018-03-27 00:06:08 +0700

    Full tourmoinhat

--- layout/tourmoinhat.php
+++ layout/tourmoinhat.php
@@ -9,46 +9,96 @@
 	   </div>
         
         <div class="row">
-            <div class="news col-md-4">
+            <div class="news col-md-4 col-sm-6">
                 <div class="thumb">
                     <a href="https://fb.com"><img src="https://travel.com.vn/Images/destination/tf_180314092517_608991.jpg"/></a>
                 </div>
                 <div class="title">
                     <a href="https://fb.com">Tên tour</a>
+                    <p>1.000.000 VND</p>
                     <div class="thongtin-tourmoinhat">
                         <div class="row">
                             <div class="col-md-8">04/06/2018 00:05 &nbsp;&nbsp;|&nbsp;&nbsp; 5 ngày</div>
-                            <div class="col-md-4">Còn 9 chỗ</div>
+                            <div class="col-md-4">Còn <b>9</b> chỗ</div>
                         </div>
                     </div>
                 </div>
             </div>
             
-            <div class="news col-md-4">
+            <div class="news col-md-4 col-sm-6">
                 <div class="thumb">
                     <a href="https://fb.com"><img src="https://travel.com.vn/Images/destination/tf_180314092517_608991.jpg"/></a>
                 </div>
                 <div class="title">
                     <a href="https://fb.com">Tên tour</a>
+                    <p>1.000.000 VND</p>
                     <div class="thongtin-tourmoinhat">
                         <div class="row">
                             <div class="col-md-8">04/06/2018 00:05 &nbsp;&nbsp;|&nbsp;&nbsp; 5 ngày</div>
-                            <div class="col-md-4">Còn 9 chỗ</div>
+                            <div class="col-md-4">Còn <b>9</b> chỗ</div>
                         </div>
                     </div>
                 </div>
             </div>
             
-            <div class="news col-md-4">
+            <div class="news col-md-4 col-sm-6">
                 <div class="thumb">
                     <a href="https://fb.com"><img src="https://travel.com.vn/Images/destination/tf_180314092517_608991.jpg"/></a>
                 </div>
                 <div class="title">
                     <a href="https://fb.com">Tên tour</a>
+                    <p>1.000.000 VND</p>
                     <div class="thongtin-tourmoinhat">
                         <div class="row">
                             <div class="col-md-8">04/06/2018 00:05 &nbsp;&nbsp;|&nbsp;&nbsp; 5 ngày</div>
-                            <div class="col-md-4">Còn 9 chỗ</div>
+                            <div class="col-md-4">Còn <b>9</b> chỗ</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="news col-md-4 col-sm-6">
+                <div class="thumb">
+                    <a href="https://fb.com"><img src="https://travel.com.vn/Images/destination/tf_180314092517_608991.jpg"/></a>
+                </div>
+                <div class="title">
+                    <a href="https://fb.com">Tên tour</a>
+                    <p>1.000.000 VND</p>
+                    <div class="thongtin-tourmoinhat">
+                        <div class="row">
+                            <div class="col-md-8">04/06/2018 00:05 &nbsp;&nbsp;|&nbsp;&nbsp; 5 ngày</div>
+                            <div class="col-md-4">Còn <b>9</b> chỗ</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            
+            <div class="news col-md-4 col-sm-6">
+                <div class="thumb">
+                    <a href="https://fb.com"><img src="https://travel.com.vn/Images/destination/tf_180314092517_608991.jpg"/></a>
+                </div>
+                <div class="title">
+                    <a href="https://fb.com">Tên tour</a>
+                    <p>1.000.000 VND</p>
+                    <div class="thongtin-tourmoinhat">
+                        <div class="row">
+                            <div class="col-md-8">04/06/2018 00:05 &nbsp;&nbsp;|&nbsp;&nbsp; 5 ngày</div>
+                            <div class="col-md-4">Còn <b>9</b> chỗ</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            
+            <div class="news col-md-4 col-sm-6">
+                <div class="thumb">
+                    <a href="https://fb.com"><img src="https://travel.com.vn/Images/destination/tf_180314092517_608991.jpg"/></a>
+                </div>
+                <div class="title">
+                    <a href="https://fb.com">Tên tour</a>
+                    <p>1.000.000 VND</p>
+                    <div class="thongtin-tourmoinhat">
+                        <div class="row">
+                            <div class="col-md-8">04/06/2018 00:05 &nbsp;&nbsp;|&nbsp;&nbsp; 5 ngày</div>
+                            <div class="col-md-4">Còn <b>9</b> chỗ</div>
                         </div>
                     </div>
                 </div>
