294bd535d9605f6c8642868f8eff233f7cb839b2
tanphat10197 <huynhlamtanphat123@gmail.com>
2018-03-28 16:29:45 +0700

    test csdl bằng cách load lên các option của mục tìm kiếm và tạo giao diện trang tìm kiếm tour
    test csdl bằng cách load lên các option của mục tìm kiếm và tạo giao diện trang tìm kiếm tour

---
+++ layout/timkiemtour.php
@@ -0,0 +1,48 @@
+<div class="container maintimtour">
+	<div class="row">
+			<h6 class="text-danger">DANH SÁCH TOUR KHỞI HÀNH TỪ HỒ CHÍ MINH</h6>
+	</div>
+
+	<div class="row ndtimtour">
+
+		<div class="col-md-1">
+			<div class="boxngay">
+				<p align="center"><span>28</span><br/> <!-- chổ này sẽ load ngày khở hành lên và tách ngày tháng năm ra nhé -->
+				3/2018 </p>
+			</div>
+		</div>
+
+		<div class="col-md-11 dstimtour">
+			<div class="ribbon blue"><span>TIẾT KIỆM</span></div>
+
+			<h6>Ý: Rome - Pisa - Florence - Venice - Milan. Khách sạn 4*</h6>
+
+			<div class="row">
+				<div class="col-md-3">
+					<img src="public/images/tour/an-do-nepal.jpg" title="an-do-nepal" width="100%">
+				</div>
+
+				<div class="col-md-9" style="margin-top: 20px;">
+					 <div class="row">
+						<div class="col-md-4">
+							<p><i class="fa fa-barcode"></i>&nbsp;1</p>
+							<p><i class="fa fa-calendar"></i>&nbsp;28/3/2018</p>
+						</div>
+
+						<div class="col-md-4">
+							<p><i class="fa fa-clock-o"></i>&nbsp;Giờ đi : 21:30</p>
+							<p><i class="fa fa-users"></i>&nbsp;28/3/2018</p>
+						</div>
+
+						<div class="col-md-4 btnchitiettour">
+							<h6 class="text-danger">46.690.000 VNĐ</h6>
+							<form>
+								<button type="submit" name="chitiet" class="btn btn-danger">CHI TIẾT</button>
+							</form>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
